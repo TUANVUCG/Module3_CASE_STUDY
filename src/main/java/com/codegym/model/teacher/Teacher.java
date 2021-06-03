0c8f0789959e567b3d1f20afb7b23eb3d50796fc
Tran Trung Kien <tieulong2803200@gmail.com>
2021-06-03 20:05:51 +0700

    update Model/Teacher.java

--- src/main/java/com/codegym/model/teacher/Teacher.java
+++ src/main/java/com/codegym/model/teacher/Teacher.java
@@ -1,4 +1,92 @@
 package com.codegym.model.teacher;
 
 public class Teacher {
+    private String id;
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private String dateOfBirth;
+    private String userId;
+
+
+
+    public Teacher(String id, String name, String address, String email, String phoneNumber, String dateOfBirth, String userId) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.userId = userId;
+    }
+
+    public Teacher(){
+
+    }
+
+    public Teacher(String name, String address, String email, String phoneNumber, String dateOfBirth, String userId) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
